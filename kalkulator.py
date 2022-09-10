83d90bcab9cfe219fb144bc5e89f43d05ca8a652
dhearahma7 <113223314+dhearahma7@users.noreply.github.com>
2022-09-09 15:12:11 -1200

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,20 @@
+angka1 = 14
+angka2 = 4
+
+# membuat penjumlahan 
+jumlah = angka1 + angka2
+
+# membuat pengurangan
+kurang = angka1 - angka2
+
+# membuat perkalian
+kali = angka1 * angka2
+
+# membuat pembagian
+bagi = angka1 / angka2
+
+# hasil 14 ditambah 4 sama dengan = 12
+print(f"hasil {angka1} ditambah {angka2} sama dengan = {jumlah}")
+print(f"hasil {angka1} dikurang {angka2} sama dengan = {kurang}")
+print(f"hasil {angka1} dikali {angka2} sama dengan = {kali}")
+print(f"hasil {angka1} dibagi {angka2} sama dengan = {bagi}")
