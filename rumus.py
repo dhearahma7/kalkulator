29bcbc99fc56135f8fe747935724a2c3f16f4df6
dhearahma7 <113223314+dhearahma7@users.noreply.github.com>
2022-09-14 02:18:56 -1200

    Add files via upload

---
+++ rumus.py
@@ -0,0 +1,16 @@
+#rumus balok
+p=int(input('masukan panjang balok:'))
+l=int(input('masukan lebar balok:'))
+t=int(input('masukan tinggi balok:'))
+
+volume1 = p * l * t
+
+print ("jadi volume balok adalah:", volume1 )
+
+#rumus kubus
+s=int(input('masukan sisi 1:'))
+s=int(input('masukan sisi 2:'))
+s=int(input('masukan sisi 3:'))
+
+volume2 = s * s * s
+print ("jadi volume kubus adalah:", volume2 )
